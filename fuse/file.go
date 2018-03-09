a8d9c811b18c5d940b6bc7a253d35880ef1f9458
Yuuichi Fujioka <fujioka.yuuichi@gmail.com>
2018-03-09 15:46:24 +0900

    implement file methods

--- fuse/file.go
+++ fuse/file.go
@@ -104,10 +104,11 @@ func (f *writableFile) GetAttr(out *fuse.Attr) fuse.Status {
 
 func (f *writableFile) Write(data []byte, off int64) (uint32, fuse.Status) {
 	log.Printf("Write: %s %d\n", f.Name, off)
-	if int64(len(f.data)) < off {
-		f.data = append(f.data, make([]byte, off-int64(len(f.data)))...)
+	end := off + int64(len(data))
+	if int64(len(f.data)) < end {
+		f.data = append(f.data, make([]byte, end-int64(len(f.data)))...)
 	}
-	f.data = append(f.data[:off], data...)
+	copy(f.data[off:], data)
 	return uint32(len(data)), fuse.OK
 }
 
@@ -126,9 +127,41 @@ func (f *writableFile) InnerFile() nodefs.File {
 }
 
 func (f *writableFile) Release() {
-	if !strings.HasPrefix(f.Name, ".") {
+	if strings.HasSuffix(f.Name, ".yaml") {
 		log.Printf("Relase: %s %s\n", f.Name, f.data)
 	} else {
 		log.Printf("Relase: %s\n", f.Name)
 	}
+}
+
+func (f *writableFile) Truncate(size uint64) fuse.Status {
+
+	if uint64(len(f.data)) < size {
+		f.data = append(f.data, make([]byte, size-uint64(len(f.data)))...)
+	}
+	for i := uint64(0); i < size; i++ {
+		f.data[i] = 0
+	}
+	return fuse.OK
+}
+
+func (f *writableFile) Allocate(off uint64, size uint64, mode uint32) (code fuse.Status) {
+	log.Printf("Allocate: %s\n", f.Name)
+	return fuse.ENOSYS
+}
+func (f *writableFile) Utimens(atime *time.Time, mtime *time.Time) fuse.Status {
+	log.Printf("Utimens: %s\n", f.Name)
+	return fuse.ENOSYS
+}
+func (f *writableFile) Fsync(flags int) (code fuse.Status) {
+	log.Printf("Fsync: %s\n", f.Name)
+	return fuse.ENOSYS
+}
+func (f *writableFile) Flush() fuse.Status {
+	log.Printf("Flush: %s\n", f.Name)
+	return fuse.ENOSYS
+}
+func (f *writableFile) Flock(flags int) fuse.Status {
+	log.Printf("Flock: %s\n", f.Name)
+	return fuse.ENOSYS
 }
